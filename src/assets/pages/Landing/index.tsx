bac3750faafda136b9209db6449263a19273735d
Thaynara Gonçalves <thaynaragoncalves@iMacDesenv.local>
2020-08-07 15:58:49 -0300

    alterado layout

--- src/assets/pages/Landing/index.tsx
+++ src/assets/pages/Landing/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 
 import logoImg from '../../images/logo.svg';
+import landingImg from '../../images/landing.svg';
+
+import studyIcon from '../../images/icons/study.svg';
+import giveClassesIcon from '../../images//icons/give-classes.svg';
+import purpleHeartIcon from '../../images/icons/purple-heart.svg'
+
+import './style.css'
 
 function Landing() {
     return (
         <div id="pages-landing">
-             <div id="pages-landing" className="container">
+             <div id="pages-landing-content" className="container">
                  <div className="logo-container">
                      <img src={logoImg} alt="Proffy" />
+                     <h2>Sua plataforma de estudos online.</h2>
                  </div>
+
+                <img src={landingImg} alt="" className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Ensinar"/>
+                        Ensinar 
+                    </a>
+                </div>
+                <span className="total-connections">
+                        Total de 200 conexões realizadas <img src={purpleHeartIcon} alt="Coraçao roxo"/>
+                </span>
              </div>
         </div>
     )
