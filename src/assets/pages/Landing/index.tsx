994c3b6cecf8042dd8a2e1a2e4ae01767895fb6e
Thaynara <thaynarafaria21@gmail.com>
2020-08-10 16:55:55 -0300

    feito paginação

--- src/assets/pages/Landing/index.tsx
+++ src/assets/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import logoImg from '../../images/logo.svg';
 import landingImg from '../../images/landing.svg';
 
@@ -21,14 +23,14 @@ function Landing() {
                 <img src={landingImg} alt="" className="hero-image"/>
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Ensinar"/>
                         Ensinar 
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                         Total de 200 conexões realizadas <img src={purpleHeartIcon} alt="Coraçao roxo"/>
